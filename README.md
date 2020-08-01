c15c30d9cfb932e4f2c76aa9705a6d41f2ece4b1
Oni <alix.boufflers@gmail.com>
2020-08-01 16:02:26 +0200

    redirection fix by router

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
 # groupomaniac
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
+when the backend server is also launched (follow the backend README), go to localhost:8081
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/router/index.js
+++ src/router/index.js
@@ -14,43 +14,74 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      allowAnonymous: true,
+    },
   },
   {
     path: "/login",
     name: "login",
     component: Home,
+    meta: {
+      allowAnonymous: true,
+    },
   },
   {
     path: "/signin",
     name: "Signin",
     component: Signin,
+    meta: {
+      allowAnonymous: true,
+    },
   },
   {
     path: "/forum",
     name: "Forum",
     component: Forum,
+    meta: {
+      allowAnonymous: false,
+    },
   },
   {
     path: "/posts/:id",
     name: "PostDetail",
     component: PostDetail,
+    meta: {
+      allowAnonymous: false,
+    },
   },
   {
     path: "/profil",
     name: "Profil",
     component: Profil,
+    meta: {
+      allowAnonymous: false,
+    },
   },
   {
     path: "/admin",
     name: "Admin",
     component: Admin,
-  }
+    meta: {
+      allowAnonymous: false,
+    },
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+router.beforeEach((to, from, next) => {
+  if (!to.meta.allowAnonymous && !JSON.parse(localStorage.getItem("userLog"))) {
+    next({
+      path: "/",
+      query: { redirect: to.fullPath },
+    });
+  } else {
+    next();
+  }
 });
 
 export default router;
