9028412f455efa400e162e09033feebce00c8c25
Oni <alix.boufflers@gmail.com>
2020-08-05 12:54:26 +0200

    bug fix openpopup target id

--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -3,11 +3,11 @@
 <template>
   <div id="body">
     <Header />
-    
+
     <div id="profil">
       <div id="popup" v-if="displayPopup">
-        <label for="confirmPassInput" >Confirmé le password</label>
-        <input class="confirmPassInput" title="password" type="password" v-model="oldPassword"/>
+        <label for="confirmPassInput">Confirmé le password</label>
+        <input class="confirmPassInput" title="password" type="password" v-model="oldPassword" />
         <button class="confirmPassButton" @click="confirmPassword">Valider</button>
       </div>
       <h1>Gestion de profil</h1>
@@ -39,7 +39,7 @@ export default {
   data: function () {
     return {
       user: {
-        id:"",
+        id: "",
         name: "",
         email: "",
         password: "",
@@ -72,83 +72,84 @@ export default {
     validateEmail(email) {
       return /\b[\w]+@[\w]+\.\w{2,4}\b/gi.test(String(email).toLowerCase());
     },
-    validatePassword(psd){
-      return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/gi.test(String(psd))
+    validatePassword(psd) {
+      return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/gi.test(
+        String(psd)
+      );
     },
 
-    openPopup(evt){
+    openPopup(evt) {
+      console.log("evt===>", evt);
       this.displayPopup = true;
-      this.action = evt.originalTarget.id
+      this.action = evt.target.id;
     },
 
-    confirmPassword(){
+    confirmPassword() {
       let userId = JSON.parse(localStorage.getItem("userLog")).id;
-     
-      let data = {password : this.oldPassword, userId}
-  
+
+      let data = { password: this.oldPassword, userId };
+
       UserDataService.confirmPassword(data)
-      .then( result => {
-        if (this.action == "updateprofil"){
-          this.modifyProfil() 
-        }else if(this.action == "deleteprofil"){
-          this.deleteProfil()
-        }
-        console.log(result)
-        this.displayPopup = false;
-      })
-      .catch( err => {
-        console.log(err)
-        console.log(JSON.parse(localStorage.getItem("userLog")))
-        this.displayPopup = false;
-      })
+        .then((result) => {
+          if (this.action == "updateprofil") {
+            this.modifyProfil();
+          } else if (this.action == "deleteprofil") {
+            this.deleteProfil();
+          }
+          console.log(result);
+          this.displayPopup = false;
+        })
+        .catch((err) => {
+          console.log(err);
+          console.log(JSON.parse(localStorage.getItem("userLog")));
+          this.displayPopup = false;
+        });
     },
 
     modifyProfil() {
-      
-      if(!this.user.email || this.validateEmail(this.user.email) == true ) {
-        if(!this.user.password || this.validatePassword(this.user.password) == true){
-          
+      if (!this.user.email || this.validateEmail(this.user.email) == true) {
+        if (
+          !this.user.password ||
+          this.validatePassword(this.user.password) == true
+        ) {
           let userId = JSON.parse(localStorage.getItem("userLog"));
           let changePassword = false;
-            if (!this.user.name) {
-              this.user.name = userId.name;
-            }
-            if (!this.user.email) {
-              this.user.email = userId.email;
-            }
-            if (!this.user.password) {
-              this.user.password = userId.password;
-              changePassword = false;
-            }else if(this.user.password){
-              changePassword = true
-            }
-            this.user.changePassword = changePassword
-            this.user.id = userId.id
-            this.user.token = userId.token
-            this.user.admin = userId.admin
-            UserDataService.update(userId.id, this.user)
-              .then((response) => {
-                console.log(response)
+          if (!this.user.name) {
+            this.user.name = userId.name;
+          }
+          if (!this.user.email) {
+            this.user.email = userId.email;
+          }
+          if (!this.user.password) {
+            this.user.password = userId.password;
+            changePassword = false;
+          } else if (this.user.password) {
+            changePassword = true;
+          }
+          this.user.changePassword = changePassword;
+          this.user.id = userId.id;
+          this.user.token = userId.token;
+          this.user.admin = userId.admin;
+          UserDataService.update(userId.id, this.user)
+            .then((response) => {
+              console.log(response);
 
-
-                localStorage.setItem("userLog", JSON.stringify(this.user))
-                window.alert("Votre profil à été mis à jour!");
-                this.user = {};
-                this.$router.push("/forum");
-
-              })
-              .catch((e) => {
-                console.log(e);
-                
-              });
-        }else{
-          window.alert("Veuillez rentrer un mot de passe contenant au moins: 8 charactères, une lettre, un chiffre et un charactère spécial")
+              localStorage.setItem("userLog", JSON.stringify(this.user));
+              window.alert("Votre profil à été mis à jour!");
+              this.user = {};
+              this.$router.push("/forum");
+            })
+            .catch((e) => {
+              console.log(e);
+            });
+        } else {
+          window.alert(
+            "Veuillez rentrer un mot de passe contenant au moins: 8 charactères, une lettre, un chiffre et un charactère spécial"
+          );
         }
-        
-      }else{
-        window.alert("Veuillez rentrer une adresse mail valide")
+      } else {
+        window.alert("Veuillez rentrer une adresse mail valide");
       }
-      
     },
   },
 };
@@ -222,15 +223,14 @@ h1 {
   display: flex;
   margin: 250px auto auto 90px;
   border: 1px solid black;
-  
-  }
+}
 .confirmPassButton {
   color: white;
   background-color: #091f43;
 }
 
-.confirmPassInput{
-max-width: 90%;
+.confirmPassInput {
+  max-width: 90%;
 }
 @media all and (max-width: 499px) {
   #email,
@@ -240,10 +240,9 @@ max-width: 90%;
     max-width: 93%;
   }
 
-  #popup{
+  #popup {
     margin: 250px auto auto 22.5px;
     width: 300px;
   }
 }
-
 </style>
